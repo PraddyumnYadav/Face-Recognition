4ff498b8bf50026cfc2a83b9c5aa0e5eb4751d30
Praddyumn Yadav <praddyumny@gmail.com>
2023-03-29 22:37:48 +0530

    Read the Image Given By the User.

--- main.py
+++ main.py
@@ -6,3 +6,7 @@ cascPath = sys.argv[2]
 
 # Create the haar cascade
 faceCascade = cv2.CascadeClassifier(cascPath)
+
+# Read the image
+image = cv2.imread(imagePath)
+gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
